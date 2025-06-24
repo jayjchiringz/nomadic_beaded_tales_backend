6008fd908055dbfeadde2bb2b9b473cf507c01d2
jayjchiringz <jayjchiringz@gmail.com>
2025-06-24 17:15:07 +0300

    Backend: Fixed products issue.

--- nomadic_api/urls.py
+++ nomadic_api/urls.py
@@ -1,17 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, ProductListCreate
+from .views import ProductViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView # type: ignore
 
-urlpatterns = [
-]
-
-
 router = DefaultRouter()
-router.register(r'products', ProductViewSet)
+router.register(r'products', ProductViewSet)  # <-- handles all CRUD
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('products/', ProductListCreate.as_view()),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
