e91159d2f5b91e16d79062957ce261cd72bb7750
jayjchiringz <jayjchiringz@gmail.com>
2025-06-24 07:40:00 +0300

    Backend: admin dashboard integration

--- nomadic_api/urls.py
+++ nomadic_api/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet
+from .views import ProductViewSet, ProductListCreate
+from rest_framework_simplejwt.views import TokenObtainPairView # type: ignore
+
+urlpatterns = [
+]
+
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('products/', ProductListCreate.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
