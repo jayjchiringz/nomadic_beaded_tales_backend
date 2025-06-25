0b60d244e3787a2e43d4aa38b5d8cebb2749dd87
jayjchiringz <jayjchiringz@gmail.com>
2025-06-25 20:23:49 +0300

    Backend: Rectified image rendering issue by updating wsgi.py

--- nomadic_beaded_core/wsgi.py
+++ nomadic_beaded_core/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for nomadic_beaded_core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+
+from whitenoise import WhiteNoise
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nomadic_beaded_core.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.MEDIA_ROOT, prefix='media/')
